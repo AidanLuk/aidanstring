e717118e830a03ccc667b119f1747aa6c142e022
AidanLuk <201111@learning.gsis.edu.hk>
2023-02-06 19:17:03 +0800

    Convert custom_string to aidan_string

--- code/aidanstring.c
+++ code/aidanstring.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct custom_string
+typedef struct aidan_string
 {
 	int length;
 	int bytes;
 	char *string;
-} custom_string;
+} aidan_string;
 
 int cstringlength(char *string)
 {
@@ -19,9 +19,9 @@ int cstringlength(char *string)
 	return(result);
 }
 
-custom_string *init_empty_custom_string(int bytes)
+aidan_string *init_empty_aidan_string(int bytes)
 {
-	custom_string *result = (custom_string *) malloc((sizeof *result));
+	aidan_string *result = (aidan_string *) malloc((sizeof *result));
 	result->length = 0;
 	result->bytes = bytes;
 	result->string = (char *) malloc((sizeof *(result->string))*bytes);
@@ -29,12 +29,12 @@ custom_string *init_empty_custom_string(int bytes)
 	return(result); 
 }
 
-void free_custom_string(custom_string *custom_string_struct)
+void free_aidan_string(aidan_string *aidan_string_struct)
 {
-	if(custom_string_struct != 0)
+	if(aidan_string_struct != 0)
 	{
-		free(custom_string_struct->string);
-		free(custom_string_struct);
+		free(aidan_string_struct->string);
+		free(aidan_string_struct);
 	}
 }
 
@@ -46,13 +46,13 @@ int main(int argc, char **argv)
 		printf("argv[%d] = %s\n", i, argv[i]);
 	}
 
-	custom_string program_name = {0};
+	aidan_string program_name = {0};
 	program_name.length = cstringlength(argv[0]);
 	program_name.string = argv[0];
 
-	custom_string* test_string = 0;
-	test_string = init_empty_custom_string(10);
-	free_custom_string(test_string);
+	aidan_string* test_string = 0;
+	test_string = init_empty_aidan_string(10);
+	free_aidan_string(test_string);
 
 	return(0);
 }
